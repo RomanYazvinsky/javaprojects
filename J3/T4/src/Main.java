860987f442852e3064254490d9add8733dba2ce5
RomanYazvinsky <32144682+RomanYazvinsky@users.noreply.github.com>
2017-10-30 21:50:24 +0300

    Printer EXTERMINATED

--- J3/T4/src/Main.java
+++ J3/T4/src/Main.java
@@ -5,25 +5,43 @@ public class Main {
 
 		Doctor lobanov = new Doctor("Lobanov");
 		Doctor acula = new Doctor("Dr. Acula");
-		Doctor john = new Doctor("J.D.");
 
 		Patient roma = new Patient("Roma");
 		Patient ivan4 = new Patient("Ivan IV The Terrible");
-		
-		Printer.printAddDoctor(registry.addDoctor(lobanov));
-		Printer.printAddPatient(registry.addPatient(roma));
-		Printer.printAddPatient(registry.addPatient(ivan4));
-		Printer.printDeleteTicket(registry.deleteTicket(roma, lobanov));
-		Printer.printAddTicket(registry.addTicket(roma, lobanov));
-		Printer.printAddTicket(registry.addTicket(ivan4, lobanov));
-		Printer.printAddTicket(registry.addTicket(roma, lobanov));
-		Printer.printDeleteTicket(registry.deleteTicket(roma, lobanov));
-		Printer.printDeleteTicket(registry.deleteTicket(ivan4, lobanov));
-		Printer.printAllTickets(registry.getTickets());
-		Printer.printAllDoctors(registry.getDoctorNames());
-		Printer.printAllPatients(registry.getPatientNames());
-		Printer.printPatientOfDoctorCount(registry.findPatientsOfDoctor(lobanov));
-		Printer.printPatientOfDoctorCount(registry.findPatientsOfDoctor(acula));
+
+		registry.addDoctor(lobanov);
+		registry.addPatient(roma);
+		registry.addPatient(ivan4);
+		registry.deleteTicket(roma, lobanov);
+		registry.addTicket(roma, lobanov);
+		registry.addTicket(ivan4, lobanov);
+		registry.addTicket(roma, lobanov);
+		registry.deleteTicket(roma, lobanov);
+		registry.deleteTicket(ivan4, lobanov);
+		printAllTickets(registry.getTickets());
+		printStringArray(registry.getDoctorNames());
+		printStringArray(registry.getPatientNames());
+		System.out.println("Number of patients of Lobanov: " + registry.findPatientsOfDoctor(lobanov));
+		System.out.println("Number of patients of Dr. Acula: " + registry.findPatientsOfDoctor(acula));
 	}
 
+	private static void printAllTickets(Ticket[] allTickets) {
+		if (allTickets == null) {
+			System.out.println("No tickets registred");
+		} else {
+			StringBuilder result = new StringBuilder();
+			for (int i = 0; i < allTickets.length  && allTickets[i] != null; i++) {
+				System.out.println(result.append(allTickets[i].getPatient().getName()).append(" to ").append(allTickets[i].getDoctor().getName()));
+				result.delete(0, result.length());
+			}
+		}
+	}
+
+	private static void printStringArray(String[] stringArray) {
+		System.out.println();
+		for (String patientToDoctor : stringArray) {
+			System.out.println(patientToDoctor);
+		}
+		System.out.println();
+	}
 }
