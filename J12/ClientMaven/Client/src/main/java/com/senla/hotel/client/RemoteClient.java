c6757eba3534e8e6e5bd1718b7b54beb8bf68414
RomanYazvinsky <32144682+RomanYazvinsky@users.noreply.github.com>
2018-03-04 16:59:04 +0300

    Hibernate

---
+++ J12/ClientMaven/Client/src/main/java/com/senla/hotel/client/RemoteClient.java
@@ -0,0 +1,71 @@
+package main.java.com.senla.hotel.client;
+
+import com.senla.hotel.api.internal.IMenuController;
+import com.senla.hotel.constants.Constants;
+import com.senla.hotel.constants.PropertyNames;
+import com.senla.hotel.exceptions.EmptyObjectException;
+import com.senla.hotel.properties.HotelProperties;
+import com.senla.hotel.ui.MenuController;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+
+public class RemoteClient {
+	private static Logger logger = LogManager.getLogger(RemoteClient.class);
+
+	public static void main(String[] args) {
+		RemoteClient client;
+		try {
+			client = new RemoteClient();
+			client.run();
+		} catch (ConnectException e) {
+			System.out.println("Server is offline");
+			logger.log(Level.DEBUG, e.getMessage());
+		}
+		
+	}
+
+	private IMenuController menuController;
+	private Socket socket;
+	private Integer port;
+	private ObjectOutputStream writer;
+	private ObjectInputStream reader;
+
+	public RemoteClient() throws ConnectException {
+		try {
+			port = Integer.parseInt(HotelProperties.getInstance(Constants.PATH_TO_PROPERTIES)
+					.getProperty(PropertyNames.PORT.toString()));
+			socket = new Socket(
+					HotelProperties.getInstance(Constants.PATH_TO_PROPERTIES).getProperty(PropertyNames.IP.toString()),
+					port);
+			menuController = new MenuController();
+			writer = new ObjectOutputStream(socket.getOutputStream());
+			InputStream inputStream = socket.getInputStream();
+			reader = new ObjectInputStream(inputStream);
+
+		} catch (ConnectException e) {
+			logger.log(Level.DEBUG, e.getMessage());
+			throw e;
+		} catch (IOException | EmptyObjectException | NumberFormatException e) {
+			logger.log(Level.DEBUG, e.getMessage());
+		}
+	}
+
+	public void run() {
+		try {
+			menuController.setIO(writer, reader);
+			menuController.run();
+			writer.close();
+			reader.close();
+		} catch (IOException e) {
+			logger.log(Level.DEBUG, e.getMessage());
+		}
+	}
+}
